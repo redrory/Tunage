02ed95d2e067e9de48848b1e8f1b1c00ce827f20
Rory Walker <redrory@gmail.com>
2011-09-02 19:36:29 -0500

    First Commit | Open Source Fri

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tunage::Application.config.secret_token = 'b46d1f51c5ffe188de90c10206326344cc3016e02569ee8d89a31fac7832af394552de2b7c1ea346858d2707e3cc9b428f2b9df0b107a0dea87c70a3c5f3721c'
